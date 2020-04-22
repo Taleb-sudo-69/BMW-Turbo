3bdc9ecc0b97775381ab5830399815ed308505f0
maryam039 <57525437+maryam039@users.noreply.github.com>
2020-04-22 20:18:49 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import './App.css';
+//import {home} from "./test.js";
+//import logo from './logo.svg';
+import {home} from './home.png';
+//import { Button } from '@material-ui/core';
+
+//import Image from "./background_web.png"; // Import using relative path
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <src img={home} className="App-home" alt="home"/>
+        <div className="h">
+        <button  class="h"><h3>TEST </h3></button>
+          </div>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <div className="menu">
+<ul>
+<button  class="b"><h3> LEVEL 1</h3></button>
+<button  class="m"><h3> LEVEL 2</h3></button>
+<button  class="a"><h3> LEVEL 3</h3></button>
+</ul>
+<br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+</div>
+<div className="menu">
+  <h1 style={{color: "#adadad"}}>Here is a test sentence that will determine the level of a person</h1>
+</div>
+<div className="h">
+        <button  class="h"><h3>SPEAK </h3></button>
+          </div>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br> 
+          <br>
+          </br>
+          <br>
+          </br> 
+
+
+
+
+
+    <div className="menu">
+  <h1 style={{color: "#0e2c5cff"}}>Here is a test sentence that will determine the level of a person</h1>
+</div>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<div class="maryam">
+<button class="maryam"><h3> LISTEN</h3></button>
+<button class="maryama"><h3> SPEAK</h3></button>
+</div>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+</div>
+    );
+  }
+}
+export default App;
